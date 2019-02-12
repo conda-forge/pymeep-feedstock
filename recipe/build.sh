528bd36c063d2cafd8951c146e77f210ae367718
Chris Hogan <c096h800@gmail.com>
2019-02-12 08:16:41 -0600

    Typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CPPFLAGS="-I${PREFIX}/include"
+
 ./configure --prefix="${PREFIX}" --with-libctl=no
 
 make -j ${CPU_COUNT}
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,7 +2,7 @@
 
 # OPENBLAS_NUM_THREADS=1 find python/tests -name "*.py" | sed /mpb/d | parallel "$PYTHON {}"
 
-for $fname in *.py; do
+for fname in *.py; do
     echo "Running $fname"
     OPENBLAS_NUM_THREADS=1 $PYTHON $fname
 done
