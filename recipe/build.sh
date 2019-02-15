04e9d39649236c709b5d9d2c0afe8a2360343663
Chris Hogan <c096h800@gmail.com>
2019-02-15 09:34:31 -0600

    Show test-suite.log on make check failure

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,8 @@ export CPPFLAGS="-I${PREFIX}/include"
 
 make -j ${CPU_COUNT}
 export OPENBLAS_NUM_THREADS=1
-pushd tests && make -j ${CPU_COUNT} check && popd
+pushd tests && make -j ${CPU_COUNT} check || (cat .test-suite.log && exit 1)
+popd
 make install
 
 rm ${SP_DIR}/meep/_meep.a
