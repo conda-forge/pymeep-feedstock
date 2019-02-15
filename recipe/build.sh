d5294fd3d03d5018fc38ef4410700d018cfe3b9c
Chris Hogan <c096h800@gmail.com>
2019-02-15 13:16:00 -0600

    Debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,9 @@ export CPPFLAGS="-I${PREFIX}/include"
 
 ./configure --prefix="${PREFIX}" --with-libctl=no
 
-make V=1 -j ${CPU_COUNT}
+make -j ${CPU_COUNT}
 export OPENBLAS_NUM_THREADS=1
-otool -L $SRC_DIR/src/.libs/libmeep.dylib
-pushd tests && make -j ${CPU_COUNT} check || (otool -L $SRC_DIR/tests/.libs/pw-source-ll && exit 1)
-popd
+pushd tests && make -j ${CPU_COUNT} check && popd
 make install
 
 rm ${SP_DIR}/meep/_meep.a
