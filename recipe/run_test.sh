480e78f4ada85fb805fbfe7d1a30e29c891839ed
Chris Hogan <c096h800@gmail.com>
2019-03-21 09:10:16 -0500

    Use run MPI tests

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-OPENBLAS_NUM_THREADS=1 find python/tests -name "*.py" | sed /mpb/d | parallel "$PYTHON {}"
+if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
+    for t in python/tests/*.py; do
+        if [ "$(basename $t)" != "material_dispersion.py" -a "$(basename $t)" != "mpb.py" ]; then
+            echo "Running $(basename $t)"
+            OPENBLAS_NUM_THREADS=1 ${PREFIX}/bin/mpiexec -n 2 $PYTHON $t
+        fi
+    done
+else
+    OPENBLAS_NUM_THREADS=1 find python/tests -name "*.py" | sed /mpb/d | parallel "$PYTHON {}"
+fi
