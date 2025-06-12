6f2e9dd36846dd317a7b129bab4b02cff025865c
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2025-06-11 20:15:35 -0700

    explicitly define unit tests to be excluded in pymeep_run_test.sh

--- recipe/pymeep_run_test.sh
+++ recipe/pymeep_run_test.sh
@@ -4,7 +4,7 @@ pip check || exit 1
 
 if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
     for t in python/tests/*.py; do
-        if [ "$(basename $t)" != "test_material_dispersion.py" -a "$(basename $t)" != "test_mpb.py" -a "$(basename $t)" != 'test_adjoint_*.py' ]; then
+        if [ "$(basename $t)" != "test_material_dispersion.py" -a "$(basename $t)" != "test_mpb.py" -a "$(basename $t)" != "test_adjoint_cyl.py" -a "$(basename $t)" != "test_adjoint_jax.py" -a "$(basename $t)" != "test_adjoint_solver.py" -a "$(basename $t)" != "test_adjoint_utils.py" ]; then
             echo "Running $(basename $t)"
             OPENBLAS_NUM_THREADS=1 ${PREFIX}/bin/mpiexec -n 2 $PYTHON $t
         fi
