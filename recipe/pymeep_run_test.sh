2687687dcf5ff06c3b4ce0c4be0c395c5d2e29b9
Ardavan Oskooi <ardavan.oskooi@gmail.com>
2025-06-11 17:57:07 -0700

    skip unit tests involving Autograd and JAX

--- recipe/pymeep_run_test.sh
+++ recipe/pymeep_run_test.sh
@@ -4,11 +4,11 @@ pip check || exit 1
 
 if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
     for t in python/tests/*.py; do
-        if [ "$(basename $t)" != "material_dispersion.py" -a "$(basename $t)" != "mpb.py" ]; then
+        if [ "$(basename $t)" != "test_material_dispersion.py" -a "$(basename $t)" != "test_mpb.py" -a "$(basename $t)" != 'test_adjoint_*.py' ]; then
             echo "Running $(basename $t)"
             OPENBLAS_NUM_THREADS=1 ${PREFIX}/bin/mpiexec -n 2 $PYTHON $t
         fi
     done
 else
-    OPENBLAS_NUM_THREADS=1 find python/tests -name "*.py" | sed /mpb/d | parallel "$PYTHON {}"
+    OPENBLAS_NUM_THREADS=1 find python/tests -name "*.py" | sed /mpb/d | sed /adjoint/d | parallel "$PYTHON {}"
 fi
